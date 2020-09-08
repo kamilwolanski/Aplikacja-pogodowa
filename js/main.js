4e0f59598dba26b65d7d71de422b889ef83300c9
kamilwolanski <kwolanski3@gmail.com>
2020-09-08 10:23:34 +0200

    dodanie pogody na 3 kolejne dni

--- js/main.js
+++ js/main.js
@@ -3,17 +3,31 @@ const cityInput = document.querySelector('#cityInput');
 const temph2 = document.querySelector('.temp');
 const cityNameH1 = document.querySelector('.city')
 const body = document.querySelector('body');
+const wrapper = document.querySelector('.wrapper');
+const header = document.querySelector('header');
 const feelsLikeSpan = document.querySelector('.feelsLikeSpan');
 const humiditySpan = document.querySelector('.humiditySpan');
 const pressureSpan = document.querySelector('.pressureSpan');
 const windSpan = document.querySelector('.windSpan');
 const searchBtn = document.querySelector('.searchBtn');
 
+const daysName = document.querySelectorAll('.dayName');
+const tempMain = document.querySelectorAll('.tempMain');
+const clouds = document.querySelectorAll('.cloud');
+
 const cloudsAndRain = {
     'few clouds':'fewClouds.jpg', 'scattered clouds':'scatteredClouds.jpg', 'clear sky':'clearSky.jpeg', 'overcast clouds':'darkClouds.jpeg',
     'light rain':'droprain.png', 'moderate rain':'droprain.png','shower rain':'rains.png','light intensity drizzle rain':'rains.png',
     'broken clouds':'brokenClouds.jpeg', 'light intensity shower rain':'rains.png', 'night':'nightSky.jpeg', 'thunderstorm':'storm.jpg'
     
+}
+
+const daysOfTheWeek = {
+    '0':'Niedziela', '1':'Poniedziałek', '2':'Wtorek', '3':'Środa', '4':'Czwartek', '5':'Piątek', '6':'Sobota', '7':'Niedziela'
+}
+
+const cloudsInMain = {
+    'clear sky':'fa-sun', 'light rain':'fa-cloud-rain'
 }
 const style = document.head.appendChild(document.createElement("style"));
 
@@ -53,6 +67,7 @@ const jakas = (data, city)=>{
     const differenceBetweenSetAndRise = sunSetDate - sunRiseDate; // Różnica pomiędzy zachodem, a wschodem słońca
 
     const nowTime = new Date(); // Obecna data i godzina
+  
     const toRise = nowTime - sunRiseInMili;
     const toSet = nowTime - sunSetInMili;
 
@@ -80,31 +95,61 @@ const jakas = (data, city)=>{
 
     const windSpeed = data.wind.speed;
     assignToHtml(windSpan, windSpeed);
+
+// Druga część strony (MAIN)
+
+    let dayOfTheWeek = nowTime.getDay()+1;
+
+    for(const dayName of daysName){ //Przypisanie nazwy dni tygodnia
+        dayName.textContent = daysOfTheWeek[dayOfTheWeek];
+        dayOfTheWeek++;
+    }
+
+    const latitude = data.coord.lat; //szerokość geograficzna
+    const longitude = data.coord.lon;//długość geograficzna
+    console.log(latitude, longitude);
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&
+    exclude={part}&appid=86ccf0dc68b8c1df51843f8e9bf5244f`)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        for(let i=0; i<3; i++){
+            const dailyTemp = parseInt(fromCalvinToCelcius(data.daily[i].temp.day));
+            tempMain[i].textContent = dailyTemp;
+            
+            const dailyCloud = data.daily[i].weather[0].description;
+            clouds[i].classList.add(`${cloudsInMain[dailyCloud]}`);
+        }
+    })
 }
 
 const dzienCzyNoc = (sumTimeToRiseAndSet, differenceBetweenSetAndRise, typeOfCloudsAndRain)=>{
   
     if(sumTimeToRiseAndSet > differenceBetweenSetAndRise){
-        style.innerHTML = `body::after {content: ''; position:fixed; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain['night']}) center no-repeat; background-size: cover; z-index: -1; opacity: .6;} body::before {content: ''; position:fixed; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain[typeOfCloudsAndRain]}) center no-repeat; background-size: cover; z-index: -1; opacity: .4}`;
-        body.style.background = `url(imgs/darkClouds.jpeg) center no-repeat`;
-        body.style.backgroundSize = "cover";
+        style.innerHTML = `header::after {content: ''; position:absolute; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain['night']}) center no-repeat; background-size: cover; opacity: .6;} header::before {content: ''; position:absolute; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain[typeOfCloudsAndRain]}) center no-repeat; background-size: cover; z-index: 0; opacity: .4}`;
+        header.style.background = `url(imgs/darkClouds.jpeg) center no-repeat`;
+        header.style.backgroundSize = "cover";
+        if(typeOfCloudsAndRain === 'light rain' || typeOfCloudsAndRain === 'moderate rain' || typeOfCloudsAndRain === 'shower rain' || typeOfCloudsAndRain === 'light intensity drizzle rain'|| typeOfCloudsAndRain === 'light intensity shower rain'){
+            rain(typeOfCloudsAndRain);
+        }
     }else{
-        body.style.background = `url(imgs/${cloudsAndRain[typeOfCloudsAndRain]}) center no-repeat`;
-        body.style.backgroundSize = "cover";
+        header.style.background = `url(imgs/${cloudsAndRain[typeOfCloudsAndRain]}) center no-repeat`;
+        header.style.backgroundSize = "cover";
         style.innerHTML = "";
 
         if(typeOfCloudsAndRain === 'light rain' || typeOfCloudsAndRain === 'moderate rain' || typeOfCloudsAndRain === 'shower rain' || typeOfCloudsAndRain === 'light intensity drizzle rain'|| typeOfCloudsAndRain === 'light intensity shower rain'){
             rain(typeOfCloudsAndRain);
         }else if(typeOfCloudsAndRain === 'thunderstorm'){
-            style.innerHTML = `body::before {content: ''; position:fixed; left:0;; top:0;; width:100%; height:100%; background: url(imgs/droprain.png) center no-repeat; background-size: cover; z-index: -1; opacity: .4;}`;
+            style.innerHTML = `header::before {content: ''; position:fixed; left:0;; top:0;; width:100%; height:100%; background: url(imgs/droprain.png) center no-repeat; background-size: cover; z-index: -1; opacity: .4;}`;
         }
     }
 }
 
 const rain =(typeOfRain)=>{
-    style.innerHTML = `body::before {content: ''; position:fixed; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain[typeOfRain]}) center no-repeat; background-size: cover; z-index: -1; opacity: .4;}`;
-    body.style.background = `url(imgs/darkClouds.jpeg) center no-repeat`;
-    body.style.backgroundSize = "cover";
+    style.innerHTML = `header::before {content: ''; position:absolute; left:0;; top:0;; width:100%; height:100%; background: url(imgs/${cloudsAndRain[typeOfRain]}) center no-repeat; background-size: cover; z-index: 2;}`;
+    header.style.background = `url(imgs/darkClouds.jpeg) center no-repeat`;
+    header.style.backgroundSize = "cover";
 }
 const enterCheck = ()=>{
     if(event.keyCode === 13){
